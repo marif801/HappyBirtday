140e491c4b79a72369a6e3747a3da5c49f9ce1f0
Cervix <52091230+marif801@users.noreply.github.com>
2022-06-26 22:59:35 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
 function myFunction() {
 document.getElementById("gambar").src="https://static.vecteezy.com/system/resources/previews/000/536/249/original/happy-birthday-typography-vol-2-vector.jpg"
-document.getElementById("tulisan").InerHTML="Selamat Ulang Tahun Yang Ke 15th"
+document.getElementById("tulisan").InerHTML="..."
 }
